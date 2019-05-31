0d3bb0f08830d38f18e148502c8e59e9eed88892
zhou <zhougoupil058@gmail.com>
2019-06-01 00:08:56 +0300

    connexion

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost:27017/Playlist', (err) => {
+//mongodb+srv://zhou059954:<$zhou03$$>@cluster0-iy04h.mongodb.net/test?retryWrites=true&w=majority
+//mongoose.connect('mongodb://localhost:27017/Playlist', (err) => {
+    mongoose.connect('mongodb+srv://zhou059954:zhougoupil@cluster0-iy04h.mongodb.net/taloutalou?retryWrites=true&w=majority', (err) => {
     if (!err)
         console.log('Connexion mandea.');
     else
